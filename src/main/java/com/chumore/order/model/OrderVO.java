388bdf6afbba93b63fcc3d137de83d67acc1e02a
Ariel Chien <jacinth170625@gmail.com>
2024-11-30 15:29:20 +0800

    add order bean java file

---
+++ src/main/java/com/chumore/order/model/OrderVO.java
@@ -0,0 +1,23 @@
+package com.chumore.order.model;
+
+public class OrderVO {
+	
+	private Integer ordId;
+	private Integer status;
+	
+	public Integer getOrdId() {
+		return ordId;
+	}
+	public void setOrdId(Integer ordId) {
+		this.ordId = ordId;
+	}
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
+	
+
+}
