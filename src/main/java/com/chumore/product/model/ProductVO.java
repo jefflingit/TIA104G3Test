6e7a68f9d278a6ea367082e752a2b2efb544cf90
jefflingit <jeff91304b@gmail.com>
2024-11-30 15:28:42 +0800

    add ProductVO

---
+++ src/main/java/com/chumore/product/model/ProductVO.java
@@ -0,0 +1,20 @@
+package com.chumore.product.model;
+
+public class ProductVO {
+	private Integer prdId;
+	private String prdName;
+	public Integer getPrdId() {
+		return prdId;
+	}
+	public void setPrdId(Integer prdId) {
+		this.prdId = prdId;
+	}
+	public String getPrdName() {
+		return prdName;
+	}
+	public void setPrdName(String prdName) {
+		this.prdName = prdName;
+	}
+	
+	
+}
