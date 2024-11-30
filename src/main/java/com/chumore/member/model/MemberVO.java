033f3d79104524d289724a3188c555306b0da99c
jefflingit <jeff91304b@gmail.com>
2024-11-30 14:50:48 +0800

    First commit

---
+++ src/main/java/com/chumore/member/model/MemberVO.java
@@ -0,0 +1,20 @@
+package com.chumore.member.model;
+
+public class MemberVO {
+	private  Integer memId;
+	private String memName;
+	public Integer getMemId() {
+		return memId;
+	}
+	public void setMemId(Integer memId) {
+		this.memId = memId;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	
+
+}
