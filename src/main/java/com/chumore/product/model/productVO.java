1a76ca601b783e6359e31192f42bd1861d7d33dd
ChenXunChang <jason86118@gmail.com>
2024-11-30 15:30:14 +0800

    add product beam java file

---
+++ src/main/java/com/chumore/product/model/productVO.java
@@ -0,0 +1,20 @@
+package com.chumore.product.model;
+
+public class productVO {
+	private  Integer productId;
+	private String productName;
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
+	
+}
